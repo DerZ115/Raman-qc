1f7aabd034d925c7b132b9e8f19a45f5f2478c71
Daniel Zimmermann <daniel.zimmermann.dz@gmail.com>
2021-03-25 15:08:28 +0100

    Added argparse & options for scoring

--- spectra_qc.py
+++ spectra_qc.py
@@ -10,20 +10,8 @@ import numpy as np
 import seaborn as sns
 from scipy.linalg import solve_banded
 from scipy.signal import argrelmin, savgol_filter
+from scipy.integrate import simpson
 
-
-# parser = argparse.ArgumentParser()
-
-# parser.add_argument()
-
-path = "spectra3"
-limit_low = 300
-limit_high = 1600
-penalty = 1
-buckets = 400
-half_width = 10
-iterations = 6
-sg_window = 35
 
 
 def importFile(path, limit_low=None, limit_high=None):
@@ -233,17 +221,18 @@ def suppression(y_subs, buckets, its, windows):
 
 
 def peakFill_4S(y, pen, hwi, its, buckets):
-    """[summary]
+    """Baseline correction as outlined in Liland, K. H. (2015) ‘4S Peak Filling - Baseline estimation by iterative mean suppression’, 
+    MethodsX. Elsevier, 2, pp. 135–140. doi: 10.1016/j.mex.2015.02.009.
 
     Args:
-        y ([type]): [description]
-        pen ([type]): [description]
-        hwi ([type]): [description]
-        its ([type]): [description]
-        buckets ([type]): [description]
+        y (np.ndarray): Array of y values of the spectra for which the baseline should be estimated. Each row should represent one spectrum
+        pen (int): Penalty to the 2nd derivative for the smoothing algorithm. See "_prep_smoothing_matrix"
+        hwi (int): Initial half-width (in buckets) of the suppression algorithm
+        its (int): Number of iterations of the suppression algorithm
+        buckets (int or list/1D-array): Either the number of buckets, or a list of bucket positions.
 
     Returns:
-        [type]: [description]
+        y_corrected (np.ndarray): Baseline corrected y values
     """
 
     dims = np.shape(y)
@@ -265,7 +254,7 @@ def peakFill_4S(y, pen, hwi, its, buckets):
     return y_corrected
 
 
-def peakRecognition(y, sg_window):
+def peakRecognition(x, y, sg_window):
     """[summary]
 
     Args:
@@ -279,41 +268,90 @@ def peakRecognition(y, sg_window):
     corrected_sg2 = savgol_filter(
         y, window_length=sg_window, polyorder=3, deriv=2)
 
-    scores = []
-    med_heights_all = []
-    n_peaks_all = []
+    peaks_all = []
 
     for i, row in enumerate(corrected_sg2):
         threshold = 0.05 + np.max(y[i])/30000
     #     print(i, threshold)
-        peaks_tmp = argrelmin(row)[0]
-        peaks_tmp = [peak for peak in peaks_tmp if row[peak] < -threshold]
+        peaks = argrelmin(row)[0]
+        peaks = [peak for peak in peaks if row[peak] < -threshold]
 
         peak_condensing = []
-        peaks_tmp2 = []
+        peaks_condensed = []
         for j in range(len(row)):
-            if j in peaks_tmp:
+            if j in peaks:
                 peak_condensing.append(j)
             if row[j] > 0 and len(peak_condensing) > 0:
-                peaks_tmp2.append(int(np.mean(peak_condensing)))
+                peaks_condensed.append(int(np.mean(peak_condensing)))
                 peak_condensing = []
         if len(peak_condensing) > 0:
-            peaks_tmp2.append(int(np.mean(peak_condensing)))
+            peaks_condensed.append(int(np.mean(peak_condensing)))
 
-        heights = [y[i, k] for k in peaks_tmp2]
-        med_heights = np.median(heights)
-        n_peaks = len(heights)
-        score = med_heights ** (n_peaks/10)
-        scores.append(score)
-        med_heights_all.append(med_heights)
-        n_peaks_all.append(n_peaks)
-
+        peaks_all.append(peaks_condensed)
         bar3.update(bar3.value + 1)
 
-    return scores, med_heights_all, n_peaks_all
+    return peaks_all
+
+
+def calc_scores(x, y, peaks, score_measure, n_peaks_influence):
+    """[summary]
+
+    Args:
+        x ([type]): [description]
+        y ([type]): [description]
+        peaks ([type]): [description]
+        score_measure ([type]): [description]
+        n_peaks_influence ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
+
+    scores = []
+    n_peaks_all = []
+
+    for i, row in enumerate(peaks):
+        n_peaks = len(row)
+        if score_measure == 0:
+            score = 1
+        elif score_measure == 1:  # median height
+            heights = [y[i, k] for k in row]
+            score = np.median(heights)
+        elif score_measure == 2:  # mean height
+            heights = [y[i, k] for k in row]
+            score = np.mean(heights)
+        elif score_measure == 3:  # mean area
+            score = simpson(y[i], x[i]) / n_peaks
+        elif score_measure == 4:  # mean area
+            score = simpson(y[i], x[i])
+
+        scores.append(score)
+        n_peaks_all.append(n_peaks)
+
+        if n_peaks_influence == 0:
+            scores_peaks = scores
+        elif n_peaks_influence == 1:
+            scores_peaks = [n*score for n, score in zip(n_peaks_all, scores)]
+        elif n_peaks_influence == 2:
+            scores_peaks = [score**(n/np.mean(n_peaks_all))
+                            for n, score in zip(n_peaks_all, scores)]
+
+        bar4.update(bar4.value + 1)
+
+    return scores_peaks, scores, n_peaks_all
 
 
 def export_sorted(path, files, scores, x, y_corr):
+    """[summary]
+
+    Args:
+        path ([type]): [description]
+        files ([type]): [description]
+        scores ([type]): [description]
+        x ([type]): [description]
+        y_corr ([type]): [description]
+    """    
+
     dest_raw = os.path.join(path, "sorted_spectra")
     dest_corr = os.path.join(path, "baseline_corrected")
 
@@ -322,11 +360,11 @@ def export_sorted(path, files, scores, x, y_corr):
     if not os.path.exists(dest_corr):
         os.mkdir(dest_corr)
 
-    files_sorted = [item[1] for item in sorted(zip(scores, files))]
+    files_sorted = [item for item in sorted(zip(scores, files))]
     files_sorted.reverse()
 
     for i in range(len(files_sorted)):
-        file = files_sorted[i]
+        file = files_sorted[i][1]
         src_file = os.path.join(path, file)
         dest_raw_file = os.path.join(dest_raw, file)
         new_file = str(i+1) + "_" + file
@@ -345,57 +383,94 @@ def export_sorted(path, files, scores, x, y_corr):
         with open(dest_corr_file, "w+") as f:
             for j in range(len(x[i_orig])):
                 f.write(str(x[i_orig, j]) + "," + str(y_corr[i_orig, j]))
-        
-        bar4.update(bar4.value + 1)
+
+        bar5.update(bar5.value + 1)
+    
+    return files_sorted
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Sort spectra by quality score using 4S baseline correction and Savitzky Golay based peak detection")
+
+    parser.add_argument("DIR", help="directory containing the spectra")
+    parser.add_argument("-l", "--limits", metavar=("LOW", "HIGH"), type=float, nargs=2, default=[None, None], help="Set limits to reduce the range of x-values")
+    parser.add_argument("-p", "--penalty", type=int, default=0, help="Penalty to the 2nd derivative used for smoothing; higher -> stronger smoothing")
+    parser.add_argument("-b", "--buckets", type=int, default=400, help="Number of buckets used for subsampling")
+    parser.add_argument("-w", "--halfwidth", type=int, default = 10, help="Initial half width for the peak suppression algorithm, in number of buckets")
+    parser.add_argument("-i", "--iterations", type=int, default=6, help="Number of iterations for the peak suppression algorithm")
+    parser.add_argument("-W", "--sgwindow", type=int, default=35, help="Window width used for smoothing before detecting peaks.")
+    parser.add_argument("-s", "--score", type=int, choices={0,1,2,3,4}, default=1, help="Measure to use for scoring spectra; 0: None, use 1 as the base score; 1: Median peak height; 2: Mean peak height; 3: Mean peak area; 4: Total peak area")
+    parser.add_argument("-n", "--npeaks", type=int, choices={0,1,2}, default=1, help="How the number of peaks influences the score; 0: No influence; 1: Multiplicative, 2: Exponential")
+
+    args = parser.parse_args()
+
     start_time = time.perf_counter()
 
-    n_files = len([file for file in os.listdir(path) if file.lower().endswith(".txt")])
+    n_files = len([file for file in os.listdir(args.DIR)
+                   if file.lower().endswith(".txt")])
 
     bar1 = progressbar.ProgressBar(
         prefix='Importing Files.......',
         max_value=n_files)
 
-    x, y, files = importDirectory(path, limit_low, limit_high)
+    x, y, files = importDirectory(args.DIR, args.limits[0], args.limits[1])
 
     bar1.finish()
     bar2 = progressbar.ProgressBar(
         prefix='Estimating Baseline...',
         max_value=n_files)
 
-    y_corrected = peakFill_4S(y, penalty, half_width, iterations, buckets)
+    y_corrected = peakFill_4S(y, args.penalty, args.halfwidth, args.iterations, args.buckets)
 
     bar2.finish()
     bar3 = progressbar.ProgressBar(
         prefix='Detecting Peaks.......',
         max_value=n_files)
 
-    scores, heights, peaks = peakRecognition(y_corrected, sg_window)
+    peaks = peakRecognition(x, y_corrected, args.sgwindow)
 
     bar3.finish()
     bar4 = progressbar.ProgressBar(
+        prefix='Calculating Scores....',
+        max_value=n_files)
+
+    scores_peaks, scores, n_peaks = calc_scores(
+        x, y_corrected, peaks, args.score, args.npeaks)
+
+    bar4.finish()
+    bar5 = progressbar.ProgressBar(
         prefix='Exporting Data........',
         max_value=n_files)
 
-    export_sorted(path, files, scores, x, y_corrected)
+    files_sorted = export_sorted(args.DIR, files, scores_peaks, x, y_corrected)
 
-    bar4.finish()
+    bar5.finish()
+
+
+    row_format ="{:<25} {:<25}"
+    print("="*50)
+    print(row_format.format("File", "Score"))
+    print("_"*50)
+    for score, file in files_sorted:
+        print(row_format.format(file, int(score)))
+    print("="*50)
 
     end_time = time.perf_counter()
 
-    print(f"Analyzed {len(files)} files in {round(end_time-start_time, 2)} seconds.")
+    print(
+        f"Analyzed {len(files)} files in {round(end_time-start_time, 2)} seconds.")
 
     sns.set(style="ticks")
 
-    fig, ((ax_box1, ax_box2),(ax_hist1, ax_hist2)) = plt.subplots(2, 2, sharex="col", gridspec_kw={"height_ratios": (.15, .85)})
+    fig, ((ax_box1, ax_box2), (ax_hist1, ax_hist2)) = plt.subplots(
+        2, 2, sharex="col", gridspec_kw={"height_ratios": (.15, .85)})
 
-    sns.boxplot(x=peaks, ax=ax_box1)
-    sns.boxplot(x=heights, ax=ax_box2)
-    sns.histplot(peaks, ax=ax_hist1)
-    sns.histplot(heights, ax=ax_hist2)
-    
+    sns.boxplot(x=n_peaks, ax=ax_box1)
+    sns.boxplot(x=scores, ax=ax_box2)
+    sns.histplot(n_peaks, ax=ax_hist1)
+    sns.histplot(scores, ax=ax_hist2)
+
     ax_box1.set(yticks=[])
     ax_box2.set(yticks=[])
     sns.despine(ax=ax_hist1)
@@ -403,8 +478,14 @@ if __name__ == '__main__':
     sns.despine(ax=ax_box1, left=True)
     sns.despine(ax=ax_box2, left=True)
 
+    score_names = {0: "No Score",
+                   1: "Median Height",
+                   2: "Mean Height",
+                   3: "Mean Area",
+                   4: "Total Area"}
+
     ax_hist1.set_xlabel("Number of Peaks")
-    ax_hist2.set_xlabel("Median Peak Height")
+    ax_hist2.set_xlabel(score_names[args.score])
 
     ax_box1.tick_params(axis="x", labelbottom=True)
     ax_box2.tick_params(axis="x", labelbottom=True)
